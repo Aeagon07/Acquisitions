95d2164d43a82f44fd118c8f779748c973191f5b
Rushikesh <godaserushikesh07@gmail.com>
2026-04-10 01:10:52 +0530

    setup the logger and middleware

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(helmet()); // this secure Express apps with various HTTP headers
+app.use(cors());
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// It is uses to parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+
+app.use(cookieParser());
+
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
+// morgain is an HTTP request logger middleware for node.js.
+
 
 app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Acquisitions API is running',
-  });
+    logger.info('Hello From Acquisitions API');
+    res.status(200).json({
+        message: 'Acquisitions API is running',
+    });
 });
 
 export default app;
